061e6be4873dff85f6c3628d799622a2a0e492f0
adityagarud <garudaditya079@gmail.com>
2026-02-05 11:52:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>
+        hello <span className="rainbow-text">bitches</span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
